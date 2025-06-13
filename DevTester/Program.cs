97ac6cccb7382a7b5870df736c0560f7e466678e
Juan Diego Gonzalez Viquez <jgonzalezvi@ucenfotec.ac.cr>
2025-06-12 18:11:44 -0600

    Se genera PR para la pelicula y se prueba insertar datos en BD.

--- DevTester/Program.cs
+++ DevTester/Program.cs
@@ -6,7 +6,7 @@ public class Program {
 
     public static void Main(string[] args) 
     {
-        var sqlOperation = new SqlOperation();
+        /*var sqlOperation = new SqlOperation();
         sqlOperation.ProcedureName = "CRE_USER_PR";
         sqlOperation.AddStringParameter("P_UserCode", "juandi");
         sqlOperation.AddStringParameter("P_Name", "Juan Diego");
@@ -16,7 +16,19 @@ public class Program {
         sqlOperation.AddStringParameter("P_Status", "AC");
 
         var sqlDAO = SqlDAO.GetInstance();
-        sqlDAO.ExecuteProcedure(sqlOperation);
+        sqlDAO.ExecuteProcedure(sqlOperation);*/
+
+        var sqlOperation2 = new SqlOperation();
+        sqlOperation2.ProcedureName = "CRE_MOVIE_PR";
+        sqlOperation2.AddStringParameter("P_Title", "Avatar 2");
+        sqlOperation2.AddStringParameter("P_Genre", "Sci-Fi");
+        sqlOperation2.AddStringParameter("P_Director", "James Cameron");
+        sqlOperation2.AddStringParameter("P_Description", "A sequel to the 2009 film Avatar.");
+        sqlOperation2.AddDateTimeParam("P_ReleaseDate", new DateTime(2022, 12, 16));
+
+        var sqlDAO2 = SqlDAO.GetInstance();
+        sqlDAO2.ExecuteProcedure(sqlOperation2);
+
 
     }
 
